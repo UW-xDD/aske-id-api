fafff2ceac9076c681a0b432915ae7b3600aa4dd
Ian Ross <iross@cs.wisc.edu>
2020-12-16 09:39:45 -0600

    add basic routes

--- src/app.py
+++ src/app.py
@@ -1,13 +1,11 @@
-from flask import Flask, request, abort, Blueprint
+from flask import Flask, request, Blueprint
 from flask import jsonify
 from flask_cors import CORS
 import psycopg2
+from psycopg2.extras import execute_values
 import logging
-import glob
 import os
-import json
-import base64
-from collections import OrderedDict
+from uuid import uuid4
 logging.basicConfig(format='%(levelname)s :: %(asctime)s :: %(message)s', level=logging.DEBUG)
 
 app = Flask(__name__)
@@ -32,7 +30,6 @@ def table_exists(cur, table_name):
             continue
     return False
 
-conn = psycopg2.
 conn = psycopg2.connect(host='aske-id-registration', user='zalando', password=os.environ["PG_PASSWORD"], sslmode='require', database='aske_id')
 cur = conn.cursor()
 
@@ -41,36 +38,57 @@ if not table_exists(cur, "registrant"):
         CREATE TABLE registrant (
             id SERIAL PRIMARY KEY,
             registrant text,
-            api_key uuid DEFAULT public.uuid_generate_v4(),
+            api_key uuid DEFAULT uuid_generate_v4()
         );""")
     conn.commit()
 if not table_exists(cur, "object"):
     cur.execute("""
         CREATE TABLE object (
-            id uuid PRIMARY KEY DEFAULT public.uuid_generate_v4(),
+            id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
             registrant_id integer REFERENCES registrant(id),
             location text DEFAULT NULL
         );""")
     conn.commit()
 
-@bp.route('/', defaults={'set_name': None})
-@bp.route('/<set_name>')
-@bp.route('/<set_name>/')
-def get_set(set_name):
-    logging.info(f"requested {request.endpoint}")
-    logging.info(set_name)
+@bp.route('/reserve')
+def reserve():
+    api_key = request.get('api_key', default=None)
+    if api_key is None:
+        return {"error" : "You must specify an API key!"}
 
-    if set_name is None:
-        results_obj = {
-                "description" : "Sets are a (still in development) construct within xDD creating a way for users to define, search, and interact with a set of documents within the xDD corpus. These sets may be defined by topic, keywords, journals, or simply a list of DOIs. Browse to any of the sets listed below (e.g. /sets/xdd-covid-19) for the set definitions along with available products created from each.",
-                "available_sets" : list(sets.keys())
-                }
-        return jsonify(results_obj)
-    else :
-        set_name = set_name.lower()
-        if set_name not in sets:
-            return {"error" : "Set undefined!"}
-        return jsonify(sets[set_name])
+    n_requested = request.get('n', default=10)
+
+    cur.execute("SELECT id FROM registrant WHERE api_key=%(api_key)", api_key)
+    registrant_id = cur.fetchone()
+    if registrant_id is None:
+        return {"error" : "Provided API key not allowed to reserve ASKE-IDs!"}
+
+    uuids = [uuid4() for i in range(n_requested)]
+
+    # INSERT INTO object n
+    # TODO : catch foreign key exception
+    execute_values(cur,
+        "INSERT INTO object (id, registrant_id) VALUES %s",
+        [(uuid, registrant_id) for uuid in uuids])
+    conn.commit()
+    return jsonify(uuids)
+
+@bp.route('/register/<oid>')
+def register(oid):
+    api_key = request.get('api_key', default=None)
+    if api_key is None:
+        return {"error" : "You must specify an API key!"}
+
+    location = request.get('location')
+    if location is None:
+        return {"error" : "You must specify a location to register this ASKE-id."}
+
+    #  check that this API key is allowed to register this oid
+    cur.execute("SELECT r.id FROM registrant r, object o WHERE o.registrant_id=r.id AND r.api_key=%(api_key) AND o.id=oid", {"api_key" : api_key, "oid" : oid})
+    registrant_id = cur.fetchone()
+    if registrant_id is None:
+        return {"error" : "Provided API key not allowed to register this ASKE-ID!"}
+    cur.execute("UPDATE object SET location=%(location) WHERE o.id=%(oid)", {"location" : location, "oid": oid})
 
 if 'PREFIX' in os.environ:
     logging.info(f"Stripping {os.environ['PREFIX']}")
